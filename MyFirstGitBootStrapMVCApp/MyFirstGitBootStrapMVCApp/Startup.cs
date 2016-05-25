d3a2b7d749da21619e47388bbcbf9a1edb1ddb15
johnvarghesemv <johnvarghesemv@gmail.com>
2016-05-26 02:07:46 +0530

    First Commit New Project

---
+++ MyFirstGitBootStrapMVCApp/MyFirstGitBootStrapMVCApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyFirstGitBootStrapMVCApp.Startup))]
+namespace MyFirstGitBootStrapMVCApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
